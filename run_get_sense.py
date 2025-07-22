19a49b48cc4450a289370d0c11bb78ad50c40300
whettenr <whettenrmw@gmail.com>
2025-07-22 15:15:35 +0200

    edit checkpointing

--- run_get_sense.py
+++ run_get_sense.py
@@ -14,6 +14,14 @@ import torchaudio
 import torchaudio.transforms as T
 from transformers.models.seamless_m4t.feature_extraction_seamless_m4t import SeamlessM4TFeatureExtractor
 
+from torch.cuda.amp import autocast
+import torch.profiler
+import numpy as np
+from speechbrain.utils.checkpoints import Checkpointer
+import yaml
+
+from speechbrain.utils.logger import get_logger
+logger = get_logger(__name__)
 
 
 SAMPLE_RATE=16000
@@ -21,22 +29,23 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 
-def load_checkpoint(path):
-    if os.path.exists(path):
-        with open(path, "rb") as f:
-            state = pickle.load(f)
-        print(f"Resumed from batch {state['last_batch_index']}")
-        return state["results"], state["last_batch_index"]
-    else:
-        return {}, -1
 
-def save_checkpoint(path, results, last_batch_index):
-    with open(path, "wb") as f:
-        pickle.dump({
-            "results": results,
-            "last_batch_index": last_batch_index
-        }, f)
-    print(f"[Checkpoint] Saved batch {last_batch_index}, total items: {len(results)}")
+# def get_last_saved_batch(log_path):
+#     if os.path.exists(log_path):
+#         with open(log_path, "r") as f:
+#             return int(f.read().strip())
+#     return -1
+
+# def set_last_saved_batch(log_path, batch_index):
+#     with open(log_path, "w") as f:
+#         f.write(str(batch_index))
+
+def save_batch_features(audio_ids, feats, batch_index, save_dir):
+    save_path = os.path.join(save_dir, f"features_batch_{batch_index:05d}.pkl")
+    data = dict(zip(audio_ids, feats))
+    with open(save_path, "wb") as f:
+        pickle.dump(data, f)
+    print(f"[Saved] {save_path} with {len(data)} items.")
 
 # go from HF dict to SB dataloader
 def data_prep(data_dict, hparams):
@@ -56,23 +65,9 @@ def data_prep(data_dict, hparams):
     @sb.utils.data_pipeline.takes("wav")
     @sb.utils.data_pipeline.provides("sig")
     def audio_pipeline(wav):
-        feature_size = 80
-        sampling_rate = 16000
-        num_mel_bins = 80
-        padding_value = 0.0
-        stride = 2
-        
-        feature_extractor = SeamlessM4TFeatureExtractor(
-            feature_size=feature_size,
-            sampling_rate=sampling_rate,
-            num_mel_bins=num_mel_bins,
-            padding_value=padding_value,
-            stride=stride
-        )
-        
         sig = read_audio(wav["bytes"])
-        # np_wav = np.array(sig)
-        features = feature_extractor(sig,sampling_rate=16000)["input_features"][0]
+        np_wav = np.array(sig)
+        features = hparams["feature_extractor"](np_wav, sampling_rate=16000)["input_features"][0]
         return torch.Tensor(features)
 
     sb.dataio.dataset.add_dynamic_item(datasets, audio_pipeline)
@@ -107,40 +102,139 @@ def data_prep(data_dict, hparams):
     return dataloader
     
 
-
 def get_features(
-    dataloader, 
-    save_interval_min=5, 
-    ckpt_path="feat_checkpoint.pkl",
+    dataloader,
+    save_interval_min=5,
+    save_dir="feature_checkpoints",
+    log_file="last_batch.txt",
     w2v_bert=None,
     attention_pool=None,
+    ckpt_path="feature_checkpoints/ckpts",
+    profiler=False,
 ):
+    os.makedirs(save_dir, exist_ok=True)
+
     if w2v_bert is None or attention_pool is None:
         print("issue with model")
-        exit(0)
-    results, last_saved_batch = load_checkpoint(ckpt_path)
+        exit(1)
+
+    # last_saved_batch = get_last_saved_batch(os.path.join(save_dir, log_file))
     start_time = time.time()
 
-    for i, batch in enumerate(tqdm(dataloader)):
-        if i <= last_saved_batch:
-            continue  # Skip already processed batches
+    audio_id_accum = []
+    feat_accum = []
 
-        batch = batch.to(DEVICE)
-        w, wl = batch.sig
-        with torch.no_grad():
-            feats = w2v_bert(w)
-            feats = attention_pool(feats)
+    step = Step(0)
+    checkpointer = Checkpointer(ckpt_path, {"dataloader": dataloader, "step": step})
+    _ = checkpointer.recover_if_possible()
+    print(f"going from step: {step.step} to {step.end}")
+    print(ckpt_path)
+
+    if not profiler:
+        with tqdm(
+            dataloader,
+            initial=step.step,
+            dynamic_ncols=True,
+        ) as t:
+            for batch in t:
+                step()
+                w, wl = batch.sig
+                w = w.to(DEVICE)
+                with torch.no_grad():
+                    with autocast(dtype=torch.float16):
+                        feats = w2v_bert(w)
+                        feats = attention_pool(feats)
+
+                audio_id_accum.extend(batch.audio_id)
+                feat_accum.extend(feats.cpu().numpy())
+
+                if (time.time() - start_time) >= save_interval_min * 60:
+                    save_batch_features(audio_id_accum, feat_accum, step.step - 1, save_dir)
+                    # set_last_saved_batch(os.path.join(save_dir, log_file), i)
+                    _ = checkpointer.save_checkpoint(end_of_epoch = False)
+                    audio_id_accum.clear()
+                    feat_accum.clear()
+                    start_time = time.time()
+
+                if (step.end > 0) and  step.end == (step.step - 1):
+                    save_batch_features(audio_id_accum, feat_accum, step.step - 1, save_dir)
+                    _ = checkpointer.save_checkpoint(end_of_epoch = False)
+                    audio_id_accum.clear()
+                    feat_accum.clear()
+                    break
+
+        # Final save if anything remains
+        if audio_id_accum:
+            save_batch_features(audio_id_accum, feat_accum, i, save_dir)
+            # set_last_saved_batch(os.path.join(save_dir, log_file), i)
+
+    else:
+
+        with torch.profiler.profile(
+                schedule=torch.profiler.schedule(wait=1, warmup=2, active=20, repeat=1),
+                on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/no_d_update_batch_1200'),
+                record_shapes=True,
+                profile_memory=True,
+                with_stack=True
+        ) as prof:
+            for i, batch in enumerate(tqdm(dataloader)):
+
+                prof.step()
+                # UNCOMMENT
+                # if i <= last_saved_batch:
+                #     continue  # Skip already processed batches
+                batch = batch.to(DEVICE)
+                w, wl = batch.sig
+                # w, wl = batch.sig
+                # w = w.to(DEVICE)
+                with torch.no_grad():
+                    with autocast(dtype=torch.float16):
+                        feats = w2v_bert(w)
+                        feats = attention_pool(feats)
+                
+                audio_id_accum.extend(batch.audio_id)
+                feat_accum.extend(feats.cpu().numpy())
+
+                if (time.time() - start_time) >= save_interval_min * 60:
+                    save_batch_features(audio_id_accum, feat_accum, i, save_dir)
+                    set_last_saved_batch(os.path.join(save_dir, log_file), i)
+                    audio_id_accum.clear()
+                    feat_accum.clear()
+                    start_time = time.time()
+                
+                if i == 1 + 2 + 20:
+                    break
+
+        print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
+
+@sb.utils.checkpoints.register_checkpoint_hooks
+class Step:
+    def __init__(self,step=0,end=0):
+        self.step = step
+        self.end = end
+
+    def __call__(self):
+        self.step = self.step + 1
+
+    def __str__(self):
+        return f"step count: {self.step}"
         
-        results.update(dict(zip(batch.audio_id, list(feats.cpu().numpy()))))
+    @sb.utils.checkpoints.mark_as_saver
+    def _save(self, path):
+        save_dict = {
+            "step": self.step,
+            "end": self.end,
+        }
+        with open(path, "w", encoding="utf-8") as w:
+            w.write(yaml.dump(save_dict))
 
-        # Checkpoint every X minutes
-        if (time.time() - start_time) >= save_interval_min * 60:
-            save_checkpoint(ckpt_path, results, i)
-            start_time = time.time()  # Reset timer
-
-    # Final save
-    save_checkpoint(ckpt_path, results, i)
-
+    @sb.utils.checkpoints.mark_as_loader
+    def _recover(self, path, end_of_epoch):
+        del end_of_epoch
+        with open(path, encoding="utf-8") as f:
+            save_dict = yaml.safe_load(f)
+        self.step = save_dict["step"]
+        self.end = save_dict["end"]
 
 
 if __name__ == "__main__":
@@ -178,17 +272,19 @@ if __name__ == "__main__":
     attention_pool.to(DEVICE)
     attention_pool.eval()
 
+    save_dir = f"{hparams['feature_function_name']}_{hparams['tls_subset']}_{hparams['save_dir']}"
+    ckpt_path = f"{hparams['feature_function_name']}_{hparams['tls_subset']}_{hparams['save_dir']}/{hparams['ckpt_path']}"
 
-    ckpt_name = f"{hparams['feature_function_name']}_{hparams['tls_subset']}_{hparams['ckpt_path']}"
-    print(DEVICE)
-    exit(0)
     # get features
     get_features(
         dataloader=dl, 
         save_interval_min=hparams["save_int"], 
-        ckpt_path=ckpt_name,
+        save_dir=save_dir,
+        log_file="last_batch.txt",
         w2v_bert=w2v_bert,
         attention_pool=attention_pool,
+        ckpt_path=ckpt_path,
+        profiler=hparams["profiler"]
     )
 
 
